77c8c9f14e855c22c2aad356ab13aa67d650ce04
Matiz <matiz76543@gmail.com>
2021-02-17 19:46:00 +0100

    Czwarty komentarz

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ namespace ConsoleApp1
             //siema
             //elo
             //trzeci
+            //nowa galonzka
             for (int i = 2; i <= n; i++)
             {
                 sum = n1 + n2;
